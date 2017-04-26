4833d06f56b8e1e44e3ea46a62d52ae6e258a497
Nick Jordan <NJordan10@gmail.com>
2017-04-26 08:49:24 -0800

    Homework 7 and 8

--- blog_entry.rb
+++ blog_entry.rb
@@ -1,3 +1,14 @@
 class BlogEntry
 	attr_accessor :date, :author, :title, :tags, :shares
+
+	<form>
+	  <label for="title">Title: </label>
+	  <input id="title" type="text"/>
+
+	  <label for="date">Date: </label>
+	  <input id="date" type="date"/>
+
+	  <label for="content">Content: </label>
+	  <textarea id="content" type="text"/>
+
 end
